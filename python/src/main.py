63260da38bf330198992e45349e31f99f7cffb63
Carlos Fuentes <solracfs@gmail.com>
2019-01-28 21:51:16 +0100

    Added copyright header

--- python/src/main.py
+++ python/src/main.py
@@ -1,5 +1,27 @@
 #!/usr/bin/python3
 
+################################################################################
+# triplet_challenge is an application that extracts the top 3 triplet words of a
+# file
+#
+# Copyright (C) 2018 Carlos Fuentes
+#
+# This file is part of triplet_challenge.
+#
+# triplet_challenge is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# triplet_challenge is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with triplet_challenge.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
 import re
 import sys
 from collections import Counter
