abb375de1da21ebf3696c3261e6310443055c7ea
Carlos Fuentes <solracfs@gmail.com>
2019-01-28 22:56:17 +0100

    Improving regex

--- src/triplet_challenge.py
+++ src/triplet_challenge.py
@@ -33,19 +33,20 @@ def main(argv):
 
   with open(argv[1], 'r') as f:
     data = f.read().lower()
-    data = re.sub('[^0-9a-z\s\']', ' ', data, flags=re.M)
-    data = re.sub('\s+', ' ', data, flags=re.M)
 
-    words = data.split(' ')
+  data = re.sub('[^0-9a-z \']', ' ', data, flags=re.M)
+  data = re.sub('  +', ' ', data, flags=re.M)
 
-    triplets = []
-    for i in range(0, len(words) - 3):
-      key = ' '.join(words[i:i+3])
-      triplets.append(key)
+  words = data.split(' ')
 
-    c = Counter(triplets)
-    for key, count in c.most_common(3):
-      print(f'{key} - {count}')
+  triplets = []
+  for i in range(0, len(words) - 3):
+    key = ' '.join(words[i:i+3])
+    triplets.append(key)
+
+  c = Counter(triplets)
+  for key, count in c.most_common(3):
+    print(f'{key} - {count}')
 
   return 0
 
